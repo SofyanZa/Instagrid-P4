6170310d41c05a8007de1df3f2b1ce49c63e6e53
SofyanZa <soza@live.fr>
2022-02-04 17:12:24 +0100

    add outlets on controller

--- InstaGrid-P4/ViewController.swift
+++ InstaGrid-P4/ViewController.swift
@@ -13,7 +13,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // Connexion des éléments du storyboard grace au control drag
+    // Outlets : Optionnal déballé car c'est toujours le cas avec les Outlets
+    @IBOutlet weak var iconSwipe: UIImageView!
+    @IBOutlet weak var swipeLabel: UILabel!
+    @IBOutlet weak var buttonRight: UIButton!
+    @IBOutlet weak var buttonCenter: UIButton!
+    @IBOutlet weak var buttonLeft: UIButton!
+    @IBOutlet weak var topLeftImageView: UIImageView!
+    @IBOutlet weak var topRightImageView: UIImageView!
+    @IBOutlet weak var bottomLeftImageView: UIImageView!
+    @IBOutlet weak var bottomRightImageView: UIImageView!
+    
+    // Ajout d'une variable permettant de savoir si une image a été tapée
+    var viewTapped: UIImageView?
+    
 
 }
 
